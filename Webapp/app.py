7d32ab071dcd8dedd44854ee50ab961a47ede2b9
Hunter Leboeuf <hjamesleboeuf@gmail.com>
2023-04-12 15:14:57 -0400

    Add files via upload
    Fixed upload

--- Webapp/app.py
+++ Webapp/app.py
@@ -7,19 +7,20 @@ from plotly.graph_objs import *
 
 app = Dash(__name__)
 
-# will be changed later just for testing am pulling file like so, will prbably change to pulling straight from the git to make work for everyone
+# This pulls the csv file straight from github so everyone can use it
 data = pd.read_csv('https://raw.githubusercontent.com/kylekaracadag/Stocks-Prediction/main/Webapp/CompanyList.csv')
 data2 = pd.read_csv('https://raw.githubusercontent.com/kylekaracadag/Stocks-Prediction/main/Webapp/Prediction.csv')
 # layout for the divs
 app.layout = html.Div([
     #title 
-    html.H4('Ai club Stocks predictor website', id="H4"),
-    html.P("Premade Graphs", id="H3", className="des"),
+    html.H4('Stocks Prediction Data Visualization', id="H4", className="H4"),
+    html.P("Line Graphs", id="H3", className="des"),
     dcc.Dropdown(
                 id='file',
                 className="dropdown",
                 options = data['.csv File'].unique(),
                 value = 'A_data.csv'# starting selection not needed but good to have
+                ,style={'color': 'blue', 'backgroundColor': 'white'} 
             ),
     dcc.RadioItems(
         id='hovermode',
@@ -30,6 +31,7 @@ app.layout = html.Div([
     ),
     dcc.RadioItems(
         id ='Data',
+        className="hovermode",
         inline =True,
         options =['Close', 'Open', 'High'],
         value ='Close'# what the starting selection will be
@@ -40,46 +42,54 @@ app.layout = html.Div([
     dcc.Dropdown(
             options = data2['.csv'].unique(),
             value ='3-13-2023_Altered_Predictions.csv',# starting selection not needed but good to have
-            id ='file1'
+            id ='file1',
+            style={'color': 'black'},
         ),
     dcc.RadioItems(
         id='hovermode1',
+        className="hovermode",
         inline=True,
         options=['x', 'x unified', 'closest'], 
         value='x' # what the starting selection will be
     ),
     dcc.RadioItems(
             id='Data1',
+            className="hovermode",
             inline=True,
             options=['Close', 'Open', 'High'],
             value='Close'# what the starting selection will be
             ),
              
         
-    dcc.Graph(id="graph2"),
-    html.H4('Dataset for candle sticks: '),
+    dcc.Graph(id="graph2"),  
+    html.H4('', id="spacer", className="spacer"),
+    html.H4('Candle Sticks: ', className="candle", id="candle"),
     dcc.Dropdown(
         id='compare1',
         className="dropdown",
         options = data['.csv File'].unique(),
-        value = 'A_data.csv'# starting selection not needed but good to have
+        value = 'A_data.csv',# starting selection not needed but good to have
+        style={'color': 'black'},
     ),
     dcc.Checklist(
         id='toggle-rangeslider',
+        className="hovermode",
         options=[{'label': 'Include Rangeslider', 
                   'value': 'slider'}],
         value=['slider']
     ),
-   
+ 
     dcc.Graph(id="graph1"),# shows actual candle sticks 
     
     dcc.Dropdown(
         data2['.csv'].unique(),
         value= '3-13-2023_Altered_Predictions.csv',# starting selection not needed but good to have
-        id='candlestick_predictions'
+        id='candlestick_predictions',
+        style={'color': 'black'},
     ),
      dcc.Checklist(
         id='toggle-rangeslider1',
+        className="hovermode",
         options=[{'label': 'Include Rangeslider', 
                   'value': 'slider'}],
         value=['slider']
@@ -105,11 +115,21 @@ def update_Graph(mode,mode1,file,compare1):
 
     fig = px.scatter(
         df, x="Date", y=mode1, 
-        title= file + " " +  mode1 + " price")
-    # fig.add_scatter(x=df1, y=mode1)
+        title= file + " " +  mode1 + " price",
+        render_mode='svg')
+    #fig.add_scatter(x=df1, y=mode1)
     fig.update_traces(
         mode="lines", hovertemplate=None)
+    fig.update_xaxes(
+        rangebreaks=[
+            dict(bounds=["sat", "mon"]) , # hide weekends
+            dict(bounds=[16, 9.5], pattern="hour"), #hide hours outside of 9am-5pm
+        ]
+    )
+        
+    
 
+     
     fig.update_layout(
         hovermode=mode,
         paper_bgcolor="#313338", plot_bgcolor="#313338",
@@ -218,11 +238,16 @@ def update_Graph(mode,mode1,file):
     df = pd.read_csv("https://raw.githubusercontent.com/kylekaracadag/Stocks-Prediction/main/Predictions/" + file)
  
     fig = px.scatter(
-        df, x="Unnamed: 0", y=mode1, 
-        title= file + " " +  mode1 + " price")
+        df, x="Date", y=mode1, 
+        title= file + " " +  mode1 + " price", render_mode='svg')
     fig.update_traces(
         mode="lines", hovertemplate=None)
-
+    fig.update_xaxes(
+        rangebreaks=[
+            dict(bounds=["sat", "mon"]) , # hide weekends
+            dict(bounds=[16, 9.5], pattern="hour"), #hide hours outside of 9am-5pm
+        ]
+    )
     fig.update_layout(
          hovermode=mode,
         paper_bgcolor="#313338", plot_bgcolor="#313338",
